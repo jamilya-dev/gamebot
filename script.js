5995a1234b9e2cd9c4e8d7d28369e5b9821a5b47
jamilya-dev <omg.groups@yandex.ru>
2024-07-27 23:40:13 +0300

    выполнено задание 6

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 'use strict'
+
+function hiddenNumber(num) {
+  let number = prompt('Угадай число от 1 до 100');
+
+  function conditions() {
+    if (isNaN(number)) {
+      number = prompt('Введи число!');
+      conditions();
+    }
+
+    if (number === null) {
+      return
+    }
+
+    if (number === '') {
+      number = prompt('Введи число!');
+    }
+
+    if (number > num) {
+      number = prompt('Загаданное число меньше');
+      conditions();
+    } else if (number < num) {
+      number = prompt("Загаданное число больше");
+      conditions();
+    } else if (number == num) {
+      alert('Поздравляю, Вы угадали!!!')
+    }
+  }
+  return conditions;
+}
+
+const func = hiddenNumber(15);
+func();
