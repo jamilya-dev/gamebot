2458fc0190222a9e76f48e2854dbd21dd9bdd3bf
jamilya-dev <omg.groups@yandex.ru>
2024-07-28 14:46:29 +0300

    улучшение функционала

--- script.js
+++ script.js
@@ -1,35 +1,31 @@
 'use strict'
 
 function hiddenNumber(num) {
-  let number = prompt('Угадай число от 1 до 100');
+  let number = +prompt('Угадай число от 1 до 100');
 
-  function conditions() {
-    if (isNaN(number)) {
-      number = prompt('Введи число!');
+  return function conditions() {
+    if (number === null || number === 0) {
+      alert('Игра окончена');
+      return;
+    }
+
+    if (isNaN(number) || number === '') {
+      number = +prompt('Введи число!');
       conditions();
-    }
-
-    if (number === null) {
-      alert('Игра окончена')
-      return
-    }
-
-    if (number === '') {
-      number = prompt('Введи число!');
+      return;
     }
 
     if (number > num) {
-      number = prompt('Загаданное число меньше');
+      number = +prompt('Загаданное число меньше');
       conditions();
     } else if (number < num) {
-      number = prompt("Загаданное число больше");
+      number = +prompt("Загаданное число больше");
       conditions();
     } else if (number == num) {
-      alert('Поздравляю, Вы угадали!!!')
-      return
+      alert('Поздравляю, Вы угадали!!!');
+      return;
     }
   }
-  return conditions;
 }
 
 const func = hiddenNumber(15);
